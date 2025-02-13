c94b7f1f59c2c93b23467953b9e2668137521d9c
Somethea Siek <sometheasiekswx@gmail.com>
2025-02-13 12:22:42 +1100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import lean from './assets/lean.jpg'
 export default function Page() {
     const [noCount, setNoCount] = useState(0)
     const [yesPressed, setYesPressed] = useState(false)
-    const yesButtonSize = noCount * 32 + 16
+    const yesButtonSize = noCount * 16 + 20
 
     const handleNoClick = () => {
         setNoCount(noCount + 1)
@@ -23,7 +23,7 @@ export default function Page() {
     }
 
     const getNoButtonText = () => {
-        const phrases = ["No", "Pretty Please Babe?", "សូមទទួលយក", "လက်ခံပေးပါ။", "I Promise to Love You", "Please Accept My Love...", "I'll Try My Best...", "Babyyyyy???", "Sorry for everything...", "I appreciate you..."]
+        const phrases = ["No", "Pretty Please Babe?", "សូមទទួលយក", "လက်ခံပေးပါ။", "I Promise to Love You", "Please Accept My Love...", "I'll Try My Best...", "Babyyyyy???", "I love you...", "I appreciate you..."]
         return phrases[noCount % (phrases.length - 1)]
     }
 
@@ -33,19 +33,17 @@ export default function Page() {
     }
 
     return (<>
-        {yesPressed ? (
-
-            <div className={" z-0 static"}>
-                <img className={"z-0 absolute bottom-10 left-0 m-10 h-[210px] -rotate-12 rounded-md"} src={lean}/>
-                <img className={"z-0 absolute top-0 left-0 m-10 h-[160px] -rotate-6 rounded-md"} src={proposeGif}/>
-                <img className={"z-0 absolute top-0 right-0 m-10 h-[225px] rotate-12 rounded-md"} src={meSelfie}/>
-                <img className={"z-0 absolute bottom-10 right-0 m-10 h-[160px] rotate-6 rounded-md"} src={lap}/>
-            </div>) : (<div className={" z-0 static"}>
-                <img className={"z-0 absolute bottom-10 left-0 m-10 h-[175px] rotate-12 rounded-md"} src={flowers}/>
-                <img className={"z-0 absolute top-0 left-0 m-10 h-[175px] rotate-6 rounded-md"} src={hug}/>
-                <img className={"z-0 absolute top-0 right-0 m-10 h-[150px] -rotate-12 rounded-md"} src={realism}/>
-                <img className={"z-0 absolute bottom-10 right-0 m-10 h-[250px] -rotate-6 rounded-md"} src={kiss}/>
-            </div>)}
+        {yesPressed ? (<div className={"relative"}>
+            <img className={"fixed bottom-10 left-0 m-10 h-[180px] -rotate-12 rounded-md -z-10"} src={lean}/>
+            <img className={"fixed top-0 right-0 m-10 h-[140px] -rotate-6 rounded-md -z-10"} src={proposeGif}/>
+            <img className={"fixed top-0 left-0 m-10 h-[200px] rotate-12 rounded-md -z-10"} src={meSelfie}/>
+            <img className={"fixed bottom-10 right-0 m-10 h-[140px] rotate-6 rounded-md -z-10"} src={lap}/>
+        </div>) : (<div className={"relative"}>
+            <img className={"fixed bottom-16 left-0 m-10 h-[150px] rotate-12 rounded-md -z-10"} src={flowers}/>
+            <img className={"fixed top-0 left-0 m-10 h-[175px] rotate-6 rounded-md -z-10"} src={hug}/>
+            <img className={"fixed top-0 right-0 m-10 h-[125px] -rotate-12 rounded-md -z-10"} src={realism}/>
+            <img className={"fixed bottom-10 right-0 m-10 h-[200px] -rotate-6 rounded-md -z-10"} src={kiss}/>
+        </div>)}
 
         <div className="flex h-screen flex-col items-center justify-center z-10">
             {yesPressed ? (<>
@@ -53,22 +51,22 @@ export default function Page() {
                     className="-mt-40 h-[300px] rounded-lg"
                     src={jumpingCatGif}
                 />
-                <h1 className="my-4 text-4xl">Your Love is Everything to Me 🥹💗</h1>
+                <h1 className="my-4 text-2xl font-medium">Your Love is Everything to Me 🥹💗</h1>
                 <button
-                    className={`mx-4 rounded bg-black px-4 py-2 font-bold text-white hover:bg-gray-800`}
+                    className={`mx-4 rounded bg-black px-4 py-2 text-white hover:bg-gray-800`}
                     onClick={() => reset()}
                 >
                     Click to Replay
                 </button>
             </>) : (<>
                 <img
-                    className="-mt-20 h-[250px] rounded-lg"
+                    className="-mt-20 h-[150px] rounded-lg"
                     src={beggingCatGif}
                 />
-                <h1 className="my-4 text-3xl">Please be my Valentine? 🥺🙏</h1>
+                <h1 className="my-4 text-2xl font-medium">Please be my Valentine? 🥺🙏</h1>
                 <div className="flex items-center">
                     <button
-                        className={`mx-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+                        className={`mx-4 rounded bg-green-500 px-4 font-bold py-2 text-white hover:bg-green-700`}
                         style={{fontSize: yesButtonSize}}
                         onClick={() => setYesPressed(true)}
                     >
@@ -76,7 +74,7 @@ export default function Page() {
                     </button>
                     <button
                         onClick={handleNoClick}
-                        className="mr-4 rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+                        className="mr-4 rounded bg-red-500 px-4 py-2 text-white hover:bg-red-700"
                     >
                         {noCount === 0 ? "No" : getNoButtonText()} 😔
                     </button>
@@ -91,10 +89,10 @@ export default function Page() {
             className="fixed bottom-0 left-0 z-20 w-full p-4 bg-black border-t shadow-sm">
                 <span className="text-sm text-white grid grid-cols-2">
                     <p className={"text-left col-span-1"}>
-                        For The Myat Su Zaw 💕
+                        For Tara 💕
                     </p>
                     <p className={"text-right col-span-1"}>
-                        Yours forever, Somethea Siek 🤙
+                        Yours forever, Somethea 🙆‍♂️
                     </p>
                 </span>
         </footer>
